54238d1fd40fa49f632cea63649d4480c398a8f7
Israel Cena <israelsjm@gmail.com>
2022-01-16 15:19:29 -0300

    create modal element

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import Modal from "react-modal";
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+		useState(false);
+
+	function handleOpenNewTransactionModal() {
+		setIsNewTransactionModalOpen(true);
+	}
+
+	function handleCloseNewTransactionModal() {
+		setIsNewTransactionModalOpen(false);
+	}
+
 	return (
 		<>
-			<Header />
+			<Modal
+				isOpen={isNewTransactionModalOpen}
+				onRequestClose={handleCloseNewTransactionModal}
+			>
+				<h1>Nova Transação</h1>
+				<button type="button" onClick={handleCloseNewTransactionModal}>
+					Fechar
+				</button>
+			</Modal>
+			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 			<Dashboard />
+
 			<GlobalStyle />
 		</>
 	);

fa26deb43358f7b6aadedb49ace381c7ef48017f
Israel Cena <israelsjm@gmail.com>
2022-01-27 18:56:46 -0300

    create summay state

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,7 +7,26 @@ import { TransactionsContext } from "../Context/TransactionsContext";
 
 export function Summary() {
 	const { transactions } = useContext(TransactionsContext);
-	console.log(transactions);
+
+	const summary = transactions.reduce(
+		(acc, transaction) => {
+			if (transaction.type === "deposit") {
+				acc.deposit += transaction.amount;
+				acc.total += transaction.amount;
+			} else {
+				acc.withdraw += transaction.amount;
+				acc.total -= transaction.amount;
+			}
+
+			return acc;
+		},
+		{
+			deposit: 0,
+			withdraw: 0,
+			total: 0,
+		}
+	);
+	console.log(summary);
 	return (
 		<Container>
 			<div>
@@ -15,7 +34,12 @@ export function Summary() {
 					<p>Entradas</p>
 					<BiUpArrowAlt size={30} id="arrowUp" />
 				</header>
-				<strong>R$ 1000,00</strong>
+				<strong>
+					{new Intl.NumberFormat("pt-BR", {
+						style: "currency",
+						currency: "BRL",
+					}).format(summary.deposit)}
+				</strong>
 			</div>
 
 			<div>
@@ -23,7 +47,13 @@ export function Summary() {
 					<p>Saídas</p>
 					<BiDownArrowAlt size={30} id="arrowDown" />
 				</header>
-				<strong>R$ -500,00</strong>
+				<strong>
+					{summary.withdraw !== 0 ? "-" : ""}
+					{new Intl.NumberFormat("pt-BR", {
+						style: "currency",
+						currency: "BRL",
+					}).format(summary.withdraw)}
+				</strong>
 			</div>
 
 			<div className="highlight-background">
@@ -31,7 +61,12 @@ export function Summary() {
 					<p>Total</p>
 					<RiMoneyDollarCircleLine size={35} />
 				</header>
-				<strong>R$ 500,00</strong>
+				<strong>
+					{new Intl.NumberFormat("pt-BR", {
+						style: "currency",
+						currency: "BRL",
+					}).format(summary.total)}
+				</strong>
 			</div>
 		</Container>
 	);
